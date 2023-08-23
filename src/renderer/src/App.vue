881ee74b653090c651a3a5095ffaf51ac47f7bbf
xiaoxi <996094754@qq.com>
2023-08-23 23:52:19 +0800

    初始化摄像头软件项目

---
+++ src/renderer/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <Suspense>
+    <main class="">
+      <Camera />
+    </main>
+  </Suspense>
+</template>
+
+<script setup lang="ts">
+import Camera from './components/Camera.vue';
+</script>
+
+<style lang="less">
+@import './assets/css/styles.less';
+</style>
---
+++ src/renderer/src/components/Camera.vue
@@ -0,0 +1,31 @@
+<!--  -->
+<template>
+    <main>
+        <video></video>
+    </main>
+</template>
+
+<script setup lang="ts">
+// const devices = await navigator.mediaDevices.enumerateDevices();
+// console.log(devices);
+
+import { onMounted } from "vue";
+
+// Put variables in global scope to make them available to the browser console.
+onMounted(() => {
+    const constraints = {
+        audio: false,
+        video: true,
+    };
+
+    const video = document.querySelector("video")!;
+
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {
+        video.srcObject = stream;
+        video.play();
+    });
+});
+
+</script>
+<style lang="scss" scoped>
+</style>
